bad7c450691db79bda650d96e542abc92109a2d0
mahtab541994 <71431688+mahtab541994@users.noreply.github.com>
2024-01-28 01:20:14 +0500

    dashboard

--- src/pages/Nav-Footer/Nav.tsx
+++ src/pages/Nav-Footer/Nav.tsx
@@ -68,7 +68,12 @@ const Nav = () => {
 					<li>  <Link to='about-us' className="text-sm text-white hover:text-gray-200">About Us</Link></li>
 				</ul>
 				{user ?
-					<Link className="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" to="/dashboard">Dashboard</Link>
+					<Link to={'/dashboard'} >
+						<div className="dash cursor-pointer">
+							<label className='cursor-pointer'><strong>Dashboard</strong></label>
+						</div>
+					</Link>
+					// <Link className="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" to="/dashboard">Dashboard</Link>
 					:
 					<>
 						<Link className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" to='/login'>Sign In</Link>
